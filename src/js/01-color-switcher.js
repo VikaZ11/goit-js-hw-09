fd655b918059ac240a231f466b04c0365436e4ca
VikaZ11 <104565629+VikaZ11@users.noreply.github.com>
2022-10-01 11:05:20 +0200

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+stopBtn.setAttribute('disabled', '');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStartClick = () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+};
+
+const onStopClick = () => {
+  clearInterval(timerId);
+
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled')
+};
+startBtn.addEventListener('click', onStartClick);
+
+stopBtn.addEventListener('click', onStopClick);
 
